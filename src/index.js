e344bf729504e6404fc980b5b2c4a81bd9108ec9
Ola Kostenok <54282812+olakostenok@users.noreply.github.com>
2020-03-15 17:12:48 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let brackets = 0;
+    
+    for (let i = 0; i < expr.length; i++){
+        if(expr.includes("(")){
+            brackets+=1
+        }
+        if(expr.includes(")")){
+            brackets-=1
+        }
+    }
+
+    if(brackets !=0){
+        throw "ExpressionError: Brackets must be paired"
+    }
+
+	if(expr.includes("/ 0")){
+    throw "TypeError: Division by zero."
+    } else
+    return(new Function('return ' + expr)()) 
+         
 }
 
 module.exports = {
